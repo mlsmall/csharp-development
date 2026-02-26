0823670fc60d6cda47e0cfb5fede3fc8ce0c45bb
Mauro Small <uesat1@yahoo.com>
2026-02-25 20:01:32 -0500

    progress update

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 23;
+            age = 45;
+            Console.WriteLine(age);
+
+            long bigNumber = 93839473L;
+            Console.WriteLine(bigNumber);
+
+            Console.ReadLine();
+        }
+    }
+}
